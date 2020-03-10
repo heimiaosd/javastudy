06a82c93fa86b66610ace6cde8323de6cfad6a34
heimiao <2381709027@qq.com>
2020-03-11 07:03:20 +0800

    添加 github登陆

---
+++ src/main/java/com/example/demo/provider/GithubProvider.java
@@ -0,0 +1,25 @@
+package com.example.demo.provider;
+
+import com.example.demo.dto.AccessTokenDTO;
+import org.springframework.stereotype.Component;
+
+@Component
+public class GithubProvider {
+    public String getAccessToken(AccessTokenDTO atDTO){
+        public static final MediaType JSON
+                = MediaType.get("application/json; charset=utf-8");
+
+        OkHttpClient client = new OkHttpClient();
+
+        String post(String url, String json) throws IOException {
+            RequestBody body = RequestBody.create(json, JSON);
+            Request request = new Request.Builder()
+                    .url(url)
+                    .post(body)
+                    .build();
+            try (Response response = client.newCall(request).execute()) {
+                return response.body().string();
+            }
+        }
+    }
+}
